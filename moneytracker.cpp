bf1016a2e46d8ad5a277f78afe0ad811a24f9d61
NguyenDAustin <101232350+NguyenDAustin@users.noreply.github.com>
2024-06-01 22:51:15 -0700

    Update moneytracker.cpp
    recoded to be able to read from file and update old file

--- moneytracker.cpp
+++ moneytracker.cpp
@@ -1,71 +1,103 @@
-    #include <iostream>
-    #include <fstream>
-    using namespace std;
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
-    void menu()
-    {
-        cout << "******************" << endl;
-        cout << "1. Balance" << endl;
-        cout << "2. Withdraw" << endl;
-        cout << "3. Deposit" << endl;
-        cout << "4. Exit" << endl;
-        cout << "*******************" << endl;
+void menu() {
+    cout << "******************" << endl;
+    cout << "1. Balance" << endl;
+    cout << "2. Withdraw" << endl;
+    cout << "3. Deposit" << endl;
+    cout << "4. Exit" << endl;
+    cout << "*******************" << endl;
+}
+
+int calculate(int o, int te, int f, int t, int tt, int ff, int h) {
+    return (o + (te * 2) + (f * 5) + (t * 10) + (tt * 20) + (ff * 50) + (h * 100));
+}
+
+void acc(int b, int o, int te, int f, int t, int tt, int ff, int h) {
+    fstream file;
+
+    file.open("Accounts.txt", ios::out);
+    if (file.is_open()) {
+        file << "Total: $" << b << endl;
+        file << "Ones: " << o << endl;
+        file << "Twos: " << te << endl;
+        file << "Fives: " << f << endl;
+        file << "Tens: " << t << endl;
+        file << "Twenties: " << tt << endl;
+        file << "Fifties: " << ff << endl;
+        file << "Hundreds: " << h << endl;
+
+        file.close();
+    }
+}
+
+void val(int &bal, int &ones, int &twos, int &fives, int &tens, int &twenties, int &fifties, int &hundreds) {
+    fstream file;
+    file.open("Accounts.txt", ios::in);
+
+    if (file.is_open()) {
+        string line;
+
+        getline(file, line);
+        bal = stoi(line.substr(line.find("$") + 1));
+
+        getline(file, line);
+        ones = stoi(line.substr(line.find(":") + 2));
+
+        getline(file, line);
+        twos = stoi(line.substr(line.find(":") + 2));
+
+        getline(file, line);
+        fives = stoi(line.substr(line.find(":") + 2));
+
+        getline(file, line);
+        tens = stoi(line.substr(line.find(":") + 2));
+
+        getline(file, line);
+        twenties = stoi(line.substr(line.find(":") + 2));
+
+        getline(file, line);
+        fifties = stoi(line.substr(line.find(":") + 2));
+
+        getline(file, line);
+        hundreds = stoi(line.substr(line.find(":") + 2));
     }
 
-    int calculate(int& b, int o, int te, int f, int t, int tt, int ff, int h)
-    {
-        b = (o + (te*2)+ (f *5) + (t*10) + (tt*20) + (ff*50) + (h*100));
+    file.close();
+}
 
-        return b;
-    }
+int main() {
+    int bal = 0;
+    int ones = 0;
+    int twos = 0;
+    int fives = 0;
+    int tens = 0;
+    int twenties = 0;
+    int fifties = 0;
+    int hundreds = 0;
 
+    val(bal, ones, twos, fives, tens, twenties, fifties, hundreds);
 
-    int main ()
-    {
-        ifstream checkFile("Account.txt"); //checks for file
-        bool fileExists = checkFile.good(); 
-        checkFile.close();
+    int opt;
 
-        ofstream file;
+    int tones;
+    int ttwos;
+    int tfives;
+    int ttens;
+    int ttwenties;
+    int tfifties;
+    int thundreds;
 
-
-        if (!fileExists){
-            file.open("Account.txt");
-        }
-
-        ifstream inputFile("Account.txt");
-    
-
-        int opt;
-        int bal = 0;
-        int con;
-
-        int ones = 0;
-        int twos = 0;
-        int fives = 0;
-        int tens = 0;
-        int twenties = 0;
-        int fifties = 0;
-        int hundreds= 0;
-
-        int tones;
-        int ttwos;
-        int tfives;
-        int ttens;
-        int ttwenties;
-        int tfifties;
-        int thundreds;
-
-        do {
+    do {
         menu();
+        cin >> opt;
 
-        cin >> opt; 
-        system("clear");
-        
-        switch (opt){
-            case 1:
-            
-            cout << "Balence is: $" << bal << endl;
+        switch (opt) {
+        case 1:
+            cout << "Balance is: $" << bal << endl;
             cout << ones << " ones" << endl;
             cout << twos << " twos" << endl;
             cout << fives << " fives" << endl;
@@ -75,23 +107,16 @@
             cout << hundreds << " hundreds" << endl;
             cout << endl;
             break;
-        
+
         case 2:
-            cout << "withdrawing:" << endl;
-            cout <<  "ones: " << endl;
-            cin >> tones;
-            cout << "twos: " << endl;
-            cin >> ttwos;
-            cout <<  "fives: " << endl;
-            cin >> tfives;
-            cout <<  "tens: " << endl;
-            cin >> ttens;
-            cout << "twenties: " << endl;
-            cin >> ttwenties;
-            cout << "fifties: " << endl;
-            cin >> tfifties;
-            cout << "hundreds: " << endl;
-            cin >> thundreds;
+            cout << "Withdrawing:" << endl;
+            cout << "ones: "; cin >> tones;
+            cout << "twos: "; cin >> ttwos;
+            cout << "fives: "; cin >> tfives;
+            cout << "tens: "; cin >> ttens;
+            cout << "twenties: "; cin >> ttwenties;
+            cout << "fifties: "; cin >> tfifties;
+            cout << "hundreds: "; cin >> thundreds;
 
             ones -= tones;
             twos -= ttwos;
@@ -101,26 +126,19 @@
             fifties -= tfifties;
             hundreds -= thundreds;
 
-            bal = calculate(bal, ones, twos, fives, tens, twenties, fifties, hundreds);
+            bal = calculate(ones, twos, fives, tens, twenties, fifties, hundreds);
 
             break;
-        
+
         case 3:
             cout << "Depositing:" << endl;
-            cout <<  "ones: " << endl;
-            cin >> tones;
-            cout << "twos: " << endl;
-            cin >> ttwos;
-            cout <<  "fives: " << endl;
-            cin >> tfives;
-            cout <<  "tens: " << endl;
-            cin >> ttens;
-            cout << "twenties: " << endl;
-            cin >> ttwenties;
-            cout << "fifties: " << endl;
-            cin >> tfifties;
-            cout << "hundreds: " << endl;
-            cin >> thundreds;
+            cout << "ones: "; cin >> tones;
+            cout << "twos: "; cin >> ttwos;
+            cout << "fives: "; cin >> tfives;
+            cout << "tens: "; cin >> ttens;
+            cout << "twenties: "; cin >> ttwenties;
+            cout << "fifties: "; cin >> tfifties;
+            cout << "hundreds: "; cin >> thundreds;
 
             ones += tones;
             twos += ttwos;
@@ -130,28 +148,17 @@
             fifties += tfifties;
             hundreds += thundreds;
 
-            bal = calculate(bal, ones, twos, fives, tens, twenties, fifties, hundreds);
+            bal = calculate(ones, twos, fives, tens, twenties, fifties, hundreds);
 
             break;
 
         case 4:
-            cout << "thank you" << endl;
+            cout << "Thank you" << endl;
             break;
-        } 
-        } while (opt != 4);
-        
-
-        if (!fileExists)
-        {
-            file << "Total: $" << bal << endl;
-            file << "Ones: " << ones << endl;
-            file << "Twos: " << twos << endl;
-            file << "Fives: " << fives << endl;
-            file << "Tens: " << tens << endl;
-            file << "Twenties: " << twenties << endl;
-            file << "Fifties: " << fifties << endl;
-            file << "Hundreds: " << hundreds << endl;
-
-            file.close();
         }
-    }
+    } while (opt != 4);
+
+    acc(bal, ones, twos, fives, tens, twenties, fifties, hundreds);
+
+    return 0;
+}
